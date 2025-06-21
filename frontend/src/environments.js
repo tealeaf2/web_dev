539877415a990f2363811b799b5d97428ec72c91
tealeaf2 <shadowrocket2003@gmail.com>
2025-06-20 18:02:30 -0700

    removed keys

--- frontend/src/environments.js
+++ frontend/src/environments.js
@@ -1,11 +1,11 @@
 // Had to change this to a const instead so that it would work?
 // Awful practice btw
 const Env = {
-  APPLICATION_ID: "4j5XHaq1S15MdLoDqDjNxwtwKJqwEgFjswEHGIJP",
-  JAVASCRIPT_KEY: "wook2bRkXekLk6pQvMWJLSe5BWvPXgM7XjrVDvNx",
+  APPLICATION_ID: "",
+  JAVASCRIPT_KEY: "",
   SERVER_URL: "https://parseapi.back4app.com",
 
-  UNSPLASH_ACCESS_KEY: "QNwsh4op9nqBELsCMr9GLpO5P9KtgRCBbQA87lgYCiU",
-  GOOGLE_MAPS_PLATFORM_KEY: "AIzaSyBi74S86NBqVmaKcnkOYcGbwAlt-MF_AOo",
+  UNSPLASH_ACCESS_KEY: "",
+  GOOGLE_MAPS_PLATFORM_KEY: "",
 };
 export default Env;
